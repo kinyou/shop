a207b6ca542e821f0386c0f2188774d8d126c85a
wangxingyuan <nova_wangxy@126.com>
2018-02-27 19:50:00 +0800

    增加商品列表的权限限制

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index(){
+	public function __construct(){
+		$this->middleware('auth');
+	}
+
+	public function index(){
     	$products = Product::paginate(10);
 
     	return view('product.index',compact('products'));
