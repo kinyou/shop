e143653332a7e4413576e6f98df39ff3dcdacc70
wangxingyuan <nova_wangxy@126.com>
2018-03-01 13:51:00 +0800

    修复mysql的users_email_unique过长

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+    	//修复users_email_unique 过长
+	    Schema::defaultStringLength(191);
     }
 
     /**
